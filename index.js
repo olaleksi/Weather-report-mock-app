7eadc5141a26d1b090982c00b6b038037bde1c94
Fav2dev <ogunkanmifavour@gmail.com>
2026-02-13 06:49:21 -0800

    I excluded lines '7,8,21-25', so that our codebase may be  DRY. Without those lines, the app still work very fine.

--- index.js
+++ index.js
@@ -4,26 +4,14 @@ import express from 'express';
 // Importing routes
 import weatherRoute from "./routers/weatherRoute.js"
 
-//Importing dirname and filename
-import { __filename, __dirname } from './routers/weatherRoute.js';
-
-
 
 const app = express();
 const port = 3000;
 
 
-
 // Middlewares
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
-
-
-// Fetching the html form on load
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html")
-})
 
 
 // Using routes from "./routers/weatherRoute.js"
